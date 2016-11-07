7844471c3341a17e8184a171612fbc3a0d09464b
Darko Lukic <darko.lukic@gowi.rs>
2016-11-07 12:09:45 +0100

    Updated git ignore, updated settings part

--- admin/class-pressmates-core-admin.php
+++ admin/class-pressmates-core-admin.php
@@ -69,7 +69,7 @@ class Pressmates_Core_Admin {
      * @param      string    $plugin_name       The name of this plugin.
      * @param      string    $version    The version of this plugin.
      */
-    public function __construct( $plugin_name, $version ) {
+    public function __construct( $plugin_name = "", $version="" ) {
 
         $this->plugin_name = $plugin_name;
         $this->version = $version;
